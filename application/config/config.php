7f6bd249e4ba9c8acc80c09bf34b529e4cbbd93c
Jakson Rochelly <jrochelly@gmail.com>
2013-04-18 15:12:25 -0300

    Improving config file

--- application/config/config.php
+++ application/config/config.php
@@ -5,9 +5,11 @@ $config['base_url'] = ''; // Base URL including trailing slash (e.g. http://loca
 $config['default_controller'] = 'main'; // Default controller to load
 $config['error_controller'] = 'error'; // Controller used for errors (e.g. 404, 500 etc)
 
-$config['db_host'] = ''; // Database host (e.g. localhost)
-$config['db_name'] = ''; // Database name
-$config['db_username'] = ''; // Database username
-$config['db_password'] = ''; // Database password
+define('DB_ENGINE', 'mysql');  
+define('DB_HOST', 'localhost');
+define('DB_PORT', 3306);
+define('DB_USER', 'root');
+define('DB_PW', '');
+define('DB_DBNAME', 'intranet');
 
 ?>
